87563b028769dc7ac07f0ae8acbf60edd3fe14b0
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2021-02-25 12:03:44 -0500

    Modified Page.js to fetch data from backend

--- frontend/src/components/Page/Page.js
+++ frontend/src/components/Page/Page.js
@@ -14,9 +14,28 @@ const Page = () => {
     const [state, setState] = useState(initialState);
     // console.log(state);
 
+    // On state change, log state
     useEffect(() => {
         console.log(state);
     }, [state])
+
+    // componentDidMount
+    useEffect( () => {
+        const result = getOrders()
+        console.log(result)
+    }, [])
+
+    const getOrders = () => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+            fetch("localhost:3001/api/v1/orders?user_id=1", requestOptions)
+            .then(response => response.json())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
 
     const nextPage = () => {
         setState({
@@ -30,6 +49,9 @@ const Page = () => {
             page: state.page > 1 ? state.page - 1 : state.page,
         })
     }
+
+    //
+    
 
 
     return (
